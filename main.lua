c47156ceb6fb4772422f24518255de9086b75ac0
DaarkDev <60558693+DaarkDev@users.noreply.github.com>
2021-03-29 18:37:55 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ __lookup["?:"] = function()
     max_args = 2; min_args = 2; system.checkArgs();
     
     for k,v in pairs(lfs.attributes(__current[2])) do
-        print(__midblue .. k, v)
+        print(__choices["midblue"] .. k, v .. __choices["reset"])
     end
     xpcall(__main, __error)
 end
